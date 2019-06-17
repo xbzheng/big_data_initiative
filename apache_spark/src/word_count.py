0b4f6e92c9605b42906d995a88f2f61cf1485246
Ivan Zheng <ivanzheng@Ivans-MBP.T-mobile.com>
2019-06-17 00:07:43 -0400

    source code

---
+++ apache_spark/src/word_count.py
@@ -0,0 +1,10 @@
+#Author: Ivan Zheng
+#Last update: 6/17/2019
+
+import pyspark
+lines = sc.textFile("hdfs:/user/cloudera/words.txt")
+lines.count()
+words = lines.flatMap(lambda line : line.split(" "))
+tuples = words.map(lambda word : (word, 1))
+counts = tuples.reduceByKey(lambda a, b: (a + b))
+counts.coalesce(1).saveAsTextFile('hdfs:/user/cloudera/wordcount/outputDir')
